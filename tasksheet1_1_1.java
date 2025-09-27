5b87c3fdfeb64c02f20d7a000c6c596d01b016ac
Kathleen Gem Guevarra <kathleen.guevarra84@gmail.com>
2025-09-27 02:54:50 +0000

    SLM Task Sheet 1

--- tasksheet1_1_1.java
+++ tasksheet1_1_1.java
@@ -1,5 +1,5 @@
 public class tasksheet1_1_1 {
     public static void main(String[] args) {
-    System.out.println("Kamusta Mundo!"); // Display the string.
+    System.out.println("Kamusta Mundo!"); 
     }
 }
